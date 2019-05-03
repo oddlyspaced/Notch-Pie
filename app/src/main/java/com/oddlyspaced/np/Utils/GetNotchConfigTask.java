a10385450ace245f40f140b615b9f9d0afe60a46
oddlyspaced <oddlyspaced1@gmail.com>
2019-05-04 00:31:05 +0530

    Add AsyncTask class AsyncTask Class for getting notch data.

---
+++ app/src/main/java/com/oddlyspaced/np/Utils/GetNotchConfigTask.java
@@ -0,0 +1,54 @@
+package com.oddlyspaced.np.Utils;
+
+// This class does the following jobs :
+// 1. Get the device name using DeviceName class.
+// 2. Check online and get the data.
+// 3. Save the data to notchpie.data file
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class GetNotchConfigTask extends AsyncTask<Void, Void, ArrayList<String>> {
+
+    private String link;
+    private int versionCode;
+    private String deviceName;
+    private ArrayList<String> data;
+    private static final String TAG = "GetNotchConfigTask";
+
+    public GetNotchConfigTask(int versionCode, String deviceName) {
+        super();
+        this.link = link;
+        this.versionCode = versionCode;
+        this.deviceName = deviceName;
+    }
+
+    @Override
+    protected void onPreExecute() {
+    }
+
+    @Override
+    protected ArrayList<String> doInBackground(Void... voids) {
+        try {
+            URL url = new URL(link + deviceName + versionCode);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            while (true) {
+                String line = reader.readLine();
+                if (line == null)
+                    break;
+                data.add(line);
+            }
+            reader.close();
+        }
+        catch (Exception e) {
+            Log.e(TAG, e.toString());
+            e.printStackTrace();
+        }
+        return data;
+    }
+}
